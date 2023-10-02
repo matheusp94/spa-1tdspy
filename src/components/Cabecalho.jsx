fec9f208f0d3ecdc99a5429c56d5286695651bde
matheusp94 <matheus.p940@gmail.com>
2023-10-02 20:36:58 -0300

    estilizando o cabecalho com css modules

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,8 +1,19 @@
+import styles from "./Cabecalho.module.css";
+
 export default function Cabecalho(){
 
     return(
         <>
-            <h1>Cabecalho</h1>
+            <header className={styles.container}>
+                <nav>
+                    <ul>
+                        <li><a href="#"></a>Item1</li>
+                        <li><a href="#"></a>Item2</li>
+                        <li><a href="#"></a>Item3</li>
+                    </ul>
+                </nav>
+            </header>
+            <h1>App</h1>
         </>
     )
 
