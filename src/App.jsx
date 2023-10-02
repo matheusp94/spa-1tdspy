ab2b1ae53e4e0b62581e9d002c63b6d24861c9a7
matheusp94 <matheus.p940@gmail.com>
2023-10-02 20:56:26 -0300

    adicionando rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import { Outlet } from "react-router-dom";
 import Cabecalho from "./components/Cabecalho";
-import Conteudo from "./components/Conteudo";
+
 import Rodape from "./components/Rodape";
 
 export default function App(){
@@ -7,19 +8,8 @@ export default function App(){
     return(
         <>
 
-            <header>
-                <nav>
-                    <ul>
-                        <li><a href="#"></a>Item1</li>
-                        <li><a href="#"></a>Item2</li>
-                        <li><a href="#"></a>Item3</li>
-                    </ul>
-                </nav>
-            </header>
-            <h1>App</h1>
-
             <Cabecalho/>
-            <Conteudo/>
+            <Outlet/>
             <Rodape/>
 
         </>
