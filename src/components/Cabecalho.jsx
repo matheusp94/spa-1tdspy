1af2021568082cdead4822c2964bebfd23459424
matheusp94 <matheus.p940@gmail.com>
2023-10-02 21:01:32 -0300

    add rotas

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,21 +1,21 @@
+import { Link } from "react-router-dom";
 import styles from "./Cabecalho.module.css";
 
-export default function Cabecalho(){
+export default function Cabecalho() {
+  return (
+    <>
+        
+     <header className={styles.container}>
+        <nav>
+            <ul>
+                <li> <Link to="/">Home</Link> </li>
+                <li> <Link to="/produtos">Produtos</Link> </li>
+                <li> <Link to="/editar/produtos/1">Produto - 1</Link> </li>
+                <li> <Link to="/editar/produtos/2">Produto - 2</Link> </li>
+            </ul>
+        </nav>
+     </header>
 
-    return(
-        <>
-            <header className={styles.container}>
-                <nav>
-                    <ul>
-                        <li><a href="#"></a>Item1</li>
-                        <li><a href="#"></a>Item2</li>
-                        <li><a href="#"></a>Item3</li>
-                    </ul>
-                </nav>
-            </header>
-            <h1>App</h1>
-        </>
-    )
-
-
+    </>
+  )
 }
